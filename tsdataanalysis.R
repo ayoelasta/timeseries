d32d37b103e468c698db3a1a545646298a685945
AyodejiAkiwowo <ayo@elastacloud.com>
2017-10-30 14:21:27 +0000

    pload data to git

--- tsdataanalysis.R
+++ tsdataanalysis.R
@@ -6,3 +6,14 @@ library(tidyquant)
 library(modelr)
 library(gridExtra)
 library(grid)
+
+
+
+
+ggplot(retail, aes(x = day, Quantity, color = income_return)) + facet_grid(income_return ~ ., scales = "free") +
+  geom_line()
+
+
+head(retail1$day)
+
+summary(retail$day_of_week)
